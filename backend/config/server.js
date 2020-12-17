c3bf9e0beebdad964348ef4f96c4072dcddb370e
Sunny Bhadani <sunnybhadani898@gmail.com>
2020-12-17 22:13:33 +0530

    Add files via upload

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'de4360ada372e5ef4d299b5456b069c2'),
+    },
+  },
+});
